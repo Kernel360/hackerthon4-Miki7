d27a24083da11226fa87929a0e8c3ad7c13463b9
Blueharmel <dldjdtjr@gmail.com>
2025-03-12 12:36:03 +0900

    [HOTFIX] 상세 페이지 댓글 구현

--- miki7/src/main/java/com/example/miki7/movie/controller/MovieController.java
+++ miki7/src/main/java/com/example/miki7/movie/controller/MovieController.java
@@ -8,8 +8,16 @@ import com.example.miki7.movie.db.MovieEntity;
 import com.example.miki7.movie.model.DetailSearchDto;
 import com.example.miki7.movie.model.SearchDto;
 import com.example.miki7.movie.service.MovieService;
+import com.example.miki7.review.db.ReviewEntity;
+import com.example.miki7.review.model.ReviewDto;
+import com.example.miki7.review.service.ReviewService;
+import com.example.miki7.user.db.UserEntity;
+import com.example.miki7.user.model.CustomUserDetails;
+import com.example.miki7.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,6 +35,8 @@ public class MovieController {
     private final MovieService movieService;
     private final ActorService actorService;
     private final CastService castService;
+    private final UserService userService;
+    private final ReviewService reviewService;
 
     // 통합 검색 - 키워드를 가지는 영화,배우,배역을 반환
     @PostMapping(value = "/search")
@@ -58,9 +68,27 @@ public class MovieController {
     @GetMapping(value = "/result/{id}")
     public String detail(@PathVariable Long id, Model model) {
 
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+//        Long userId = user.getId();
+        UserEntity userEntity = userService.findByNickname(userDetails.getUsername()).get();
+        Long userId = userEntity.getId();
         MovieEntity movie = movieService.findMovieById(id);
+        List<CastEntity> castList = reviewService.getCastsByMovieId(id); // 해당영화 배역 목록
+        List<ReviewDto> reviews = reviewService.getReviewsByMovieId(id); // ✅ 해당 영화의 리뷰 목록 추가
+        int totalScore = 0;
+        for(ReviewDto review:reviews){
+            totalScore+=review.getReviewRating();
+        }
+        String averageScore = String.format("%.2f", Double.parseDouble(String.valueOf(totalScore)) / reviews.size());
 
-        model.addAttribute("movie", movie);
+        model.addAttribute("loginUserId",userId); // 모델에 userId 추가
+        model.addAttribute("movie", movie); // ✅ 영화 정보 추가
+        model.addAttribute("movieId", id); // 선택한 영화
+        model.addAttribute("castList", castList); // 영화에 출연한 배우/배역 목록 선택을 위해
+        model.addAttribute("review", new ReviewEntity());
+        model.addAttribute("reviews", reviews); // ✅ 영화 리뷰 목록 추가
+        model.addAttribute("avgScore",averageScore);
 
         return "post";
     }
